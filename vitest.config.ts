d9f53fb2aa3c58f07df67e47fcadfd5a72eea2db
Nico Baier <nico.baier@gmail.com>
2026-01-11 20:40:14 -0600

    chore: formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+	resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 	test: {
 		globals: false,
 		environment: "node",
